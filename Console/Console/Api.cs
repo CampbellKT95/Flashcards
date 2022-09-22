04ead3294cad236790ad7419b12392bd0dee92ad
Kadin Campbell <kadincampbell@Kadins-MBP.attlocal.net>
2022-09-22 16:33:00 -0400

    began on scrapper. fetched & parsed sentences. Need to further grab kanji

--- Console/Console/Api.cs
+++ Console/Console/Api.cs
@@ -3,12 +3,14 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using User.Models;
+using User.Tools;
 
 namespace User.Api
 {
     public class ApiInteraction
     {
         static readonly HttpClient client = new HttpClient();
+        static readonly WebScrapper scrapper = new WebScrapper();
         static readonly string urlBase = "https://localhost:7209";
 
         public async Task<List<Flashcard>> ViewCards()
@@ -27,14 +29,19 @@ namespace User.Api
 
         public async Task<string> CreateNewCard(Flashcard newCard)
         {
-            string serializedCard = JsonSerializer.Serialize(newCard);
-            StringContent content = new StringContent(serializedCard, Encoding.UTF8, "application/json");
+            //string serializedCard = JsonSerializer.Serialize(newCard);
+            //StringContent content = new StringContent(serializedCard, Encoding.UTF8, "application/json");
 
-            var response = await client.PostAsync($"{urlBase}/card", content);
+            //var response = await client.PostAsync($"{urlBase}/card", content);
 
-            string responseContent = await response.Content.ReadAsStringAsync();
+            //string responseContent = await response.Content.ReadAsStringAsync();
 
-            return responseContent;
+            //return responseContent;
+
+
+            // testing scrapper only
+            await scrapper.ScrapSentence("魚");
+            return "test";
         }
 
         public async Task<string> DeleteCard(string word)
---
+++ Console/Console/tools/WebScrapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+
+namespace User.Tools
+{
+    public class WebScrapper
+    {
+        private readonly string jishoUrl = "https://jisho.org/search/";
+
+        public async Task ScrapSentence(string word)
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync($"{jishoUrl}/{word}%23sentences");
+
+            string htmlContent = await response.Content.ReadAsStringAsync();
+
+            // find first three instances of japanese_sentence, which indicates the beginning of a jp sentence
+            int[] allSentenceStarts = new int[3];
+            int lastStartPosition = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                // search for the next sentence beginning from the last found sentence
+                int sentenceStart = htmlContent.IndexOf("japanese_sentence", lastStartPosition + 1);
+                lastStartPosition = sentenceStart;
+
+                allSentenceStarts[i] = sentenceStart;
+            }
+
+            // find the first three instances of english_sentence, which indicates the beginning of an en sentence (and thus the end of the coorsponding jp sentence)
+            int[] allSentenceEnds = new int[3];
+            int lastEndPosition = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int sentenceEnd = htmlContent.IndexOf("english_sentence", lastEndPosition + 1);
+                lastEndPosition = sentenceEnd;
+
+                allSentenceEnds[i] = sentenceEnd;
+            }
+
+            // break into smaller snippts
+            string[] allSentences = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                int snippetLength = allSentenceEnds[i] - allSentenceStarts[i];
+                string snippet = htmlContent.Substring(allSentenceStarts[i], snippetLength);
+
+                allSentences[i] = snippet;
+            }
+
+            System.Console.WriteLine(String.Join(",", allSentences));
+
+            // all kanji are fixed in a <span class="unlinked">. Parse these out into an array
+        }
+    }
+}
+
